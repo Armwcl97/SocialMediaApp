21b636c79ae836574eaed699866ba629a65abb59
armwcl97 <armandowcl97@gmail.com>
2023-06-12 21:15:48 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/api/ChatRequest.js 	new file:   src/api/MessageRequest.js 	new file:   src/components/ChatBox/ChatBox.css 	new file:   src/components/ChatBox/ChatBox.jsx 	new file:   src/components/Conversation/Conversation.jsx 	modified:   src/components/RightSide/RightSide.jsx 	new file:   src/pages/Chat/Chat.css 	new file:   src/pages/Chat/Chat.jsx 	modified:   yarn.lock

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { Routes, Route, Navigate} from "react-router-dom";
 import "./App.css"
 import Auth from "./pages/Auth/Auth";
+import Chat from "./pages/Chat/Chat";
 import Home from './pages/home/Home'
 import Profile from "./pages/profile/Profile";
 
@@ -17,6 +18,7 @@ function App() {
 
           <Route path = '/auth' element={user?<Navigate to='../home' /> : <Auth />} />
           <Route path='/profile/:id' element={user? <Profile/> : <Navigate to="../auth" />} />
+          <Route path='/chat' element={user? <Chat /> : <Navigate to="../auth" />} />
         </Routes>
     </div>
   );
